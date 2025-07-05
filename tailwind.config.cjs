23d580411c853e5e583f4bed1eaf7da116d02c10
Noussair <noussairmekni7@gmail.com>
2025-07-05 13:23:53 +0100

    Refactor Tailwind and PostCSS configuration files for improved structure and clarity

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
+const typography = require('@tailwindcss/typography');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     darkMode: 'class',
 
     content: [
